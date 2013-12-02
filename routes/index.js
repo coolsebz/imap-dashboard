0ceaaa6cafa6e8b194146048988b092083dfb0c1
Marius Sebastian Trif <coolsebz@gmail.com>
2013-12-02 23:29:02 +0200

    Tweaked jade index with mocked data

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+	user: {
+		name: "debugTest",
+		folders: ["this is one", "this is another one"],
+		emails: ["email1", "email2", "email3"]
+	} 
+	});
 };
