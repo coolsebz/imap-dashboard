0210ee8c77fa9c0d92503dc60c4ed27e8b1fb352
Marius Sebastian Trif <coolsebz@gmail.com>
2013-12-04 03:36:52 +0200

    Small allignment issue

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@
 
 exports.index = function(req, res){
   res.render('index', {
-	user: {
-		name: "debugTest",
-		folders: ["this is one", "this is another one"],
-		emails: ["email1", "email2", "email3"]
-	} 
+		user: {
+			name: "debugTest",
+			folders: ["this is one", "this is another one"],
+			emails: ["email1", "email2", "email3"]
+		} 
 	});
 };
